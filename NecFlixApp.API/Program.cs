e106ebb7f53251fd05f0326c22077eedde0c51fa
nctkrlms <75181243+nctkrlms@users.noreply.github.com>
2023-09-26 15:31:41 +0300

    bug fixes

--- NecFlixApp.API/Program.cs
+++ NecFlixApp.API/Program.cs
@@ -11,7 +11,6 @@ builder.Services.AddDbContext<MoviesAppDatabaseContext>(options => options.UseSq
 builder.Services.AddAutoMapper(typeof(MapperConfig));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
